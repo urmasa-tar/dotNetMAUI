fe9aace6a02e433d636de19ae82f6c769cfac930
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-10-29 16:50:09 +0300

    firstAplication

---
+++ TestAplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestAplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
